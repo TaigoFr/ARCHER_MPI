24e6ecbb745317006c4dfc89117d29dc3fa9a1d0
TaigoFr <taigo.fr@outlook.com>
2019-04-02 12:04:09 +0100

    update on exercise 1

--- OpenMP/HelloWorld/helloworld.c
+++ OpenMP/HelloWorld/helloworld.c
@@ -3,11 +3,13 @@
 #include <omp.h>
 
 int main(){
+    printf("OUT hello from thread %d of %d\n",omp_get_thread_num(),omp_get_num_threads());
+
 #pragma omp parallel
     {
 #pragma omp critical 
      {
-      printf("hello from thread %d\n",omp_get_thread_num());
+      printf("IN hello from thread %d of %d\n",omp_get_thread_num(),omp_get_num_threads());
      }
     }
 }
